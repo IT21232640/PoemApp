a961e7d39480a2590c73ecbd379d7f4f96bdf5ac
anuththara <it21232640@my.sliit.lk>
2024-11-27 23:11:35 +0530

    Initial Commit

---
+++ PoemApp/PoemAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoemAppApp.swift
+//  PoemApp
+//
+//  Created by Anuththara Divarathna on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct PoemAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
